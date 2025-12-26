e8fefdcf3f56e616ae6710d42a26d0cd1665e030
Tyler Haney <tyler.haney.1998@gmail.com>
2025-12-26 04:46:39 +0000

    File changes

--- functions/sendARReceiptEmail.ts
+++ functions/sendARReceiptEmail.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
 import { format } from 'npm:date-fns@2.30.0';
 
 Deno.serve(async (req) => {
